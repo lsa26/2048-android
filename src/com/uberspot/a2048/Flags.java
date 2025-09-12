dfdc629edd994f5e5584de29036c302f038854b1
lsa26 <151360195+lsa26@users.noreply.github.com>
2025-09-12 17:42:38 +0200

    Create Flags.java

---
+++ src/com/uberspot/a2048/Flags.java
@@ -0,0 +1,14 @@
+package com.uberspot.a2048;
+
+import io.rollout.configuration.RoxContainer;
+import io.rollout.flags.RoxFlag;
+import io.rollout.flags.RoxString;
+
+// Conteneur de feature flags
+public class Flags implements RoxContainer {
+    // Booléen: désactiver le tutoriel par défaut
+    public RoxFlag enableTutorial = new RoxFlag(false);
+
+    // Multivarié: couleur du titre, valeur par défaut "White"
+    public RoxString titleColors = new RoxString("White", new String[] {"White", "Blue", "Green", "Yellow"});
+}
